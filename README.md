a0f7113b4aedf277f96979bb299d8bf838beadd7
Yasunobu Okamura <okamura@informationsea.info>
2020-08-29 16:56:38 +0900

    update packages

--- README.md
+++ README.md
@@ -3,8 +3,6 @@
 [![Docker Cloud Automated build](https://img.shields.io/docker/cloud/automated/informationsea/r-essentials)](https://hub.docker.com/r/informationsea/r-essentials)
 [![Docker Cloud Build Status](https://img.shields.io/docker/cloud/build/informationsea/r-essentials)](https://hub.docker.com/r/informationsea/r-essentials)
 
-This image uses `checkpoint` for reproducibility. Please read [description](https://mran.microsoft.com/documents/rro/reproducibility) to learn more.
-
 ## Installed packages
 
 * tidyverse
@@ -15,7 +13,6 @@ This image uses `checkpoint` for reproducibility. Please read [description](http
 * knitr
 * xtable
 * jsonlite
-
 * deconstructSigs
 * BSgenome.Hsapiens.UCSC.hg19
 * vcfR
--- install.R
+++ install.R
@@ -1,9 +1,5 @@
 options(repos="http://cran.rstudio.com/")
 
-install.packages("checkpoint")
-library(checkpoint)
-checkpoint("2020-08-28")
-
 if (!requireNamespace("BiocManager", quietly = TRUE)) {
     install.packages("BiocManager")
 }
@@ -11,10 +7,9 @@ BiocManager::install("BSgenome", version = "3.11")
 BiocManager::install("BSgenome.Hsapiens.UCSC.hg19", version = "3.11")
 BiocManager::install("GenomeInfoDb", version = "3.11")
 
-install.packages("https://github.com/knausb/vcfR/archive/v1.11.0.tar.gz")
-install.packages(c("deconstructSigs"))
-# install.packages(c(
-#     "xlsx", "vcfR", "deconstructSigs",
-#     "RSQLite", "tidyverse", "shiny", "rmarkdown", "knitr"
-# ))
+install.packages(c(
+    "xlsx", "deconstructSigs", "RSQLite", "tidyverse", "shiny", "rmarkdown",
+    "knitr", "xtable", "jsonlite", "ape", "memuse", "pinfsc50", "vegan"
+))
 
+install.packages("https://github.com/knausb/vcfR/archive/v1.11.0.tar.gz")
