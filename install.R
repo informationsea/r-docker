dae2cb434db67efe6b727cbe5c5ed752e228e893
OKAMURA, Yasunobu <okamura@informationsea.info>
2021-02-01 13:29:40 +0900

    updates

--- install.R
+++ install.R
@@ -3,13 +3,11 @@ options(repos="http://cran.rstudio.com/")
 if (!requireNamespace("BiocManager", quietly = TRUE)) {
     install.packages("BiocManager")
 }
-BiocManager::install("BSgenome", version = "3.11")
-BiocManager::install("BSgenome.Hsapiens.UCSC.hg19", version = "3.11")
-BiocManager::install("GenomeInfoDb", version = "3.11")
+BiocManager::install("BSgenome")
+BiocManager::install("BSgenome.Hsapiens.UCSC.hg19")
+BiocManager::install("GenomeInfoDb")
 
 install.packages(c(
     "xlsx", "deconstructSigs", "RSQLite", "tidyverse", "shiny", "rmarkdown",
-    "knitr", "xtable", "jsonlite", "ape", "memuse", "pinfsc50", "vegan"
+    "knitr", "xtable", "jsonlite", "ape", "memuse", "pinfsc50", "vegan", "vcfR"
 ))
-
-install.packages("https://github.com/knausb/vcfR/archive/v1.11.0.tar.gz")
